a93b9e73e5ef36e82cccaa082ed2059b84b0c51a
MoBainsJP <95718489+MoBainsJP@users.noreply.github.com>
2021-12-08 02:45:02 -0500

    Delete webpersonal directory

--- webpersonal/portafolio/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Project
-
-
-# Create your views here.
-def portafolio(request):
-    proyects = Project.objects.all()
-    return render(request, "portafolio/portafolio.html", {'projects' : proyects })
